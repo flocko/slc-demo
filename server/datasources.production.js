02a410d71cd2f17a65aa020dee3deaf434ba4539
Florian Kircher <flo.kircher@gmail.com>
2015-12-10 19:24:03 +0100

    testing env

---
+++ server/datasources.production.js
@@ -0,0 +1,14 @@
+var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST;
+var mongoPort = process.env.OPENSHIFT_MONGODB_DB_PORT;
+var mongoPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+var mongoUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+
+module.exports = {
+  mongodb: {
+    url: "mongodb://" + mongoHost + ":" + mongoPort,
+    port: mongoPort,
+    database: "funcourt",
+    user: mongoUser,
+    password: mongoPass
+  }
+};
