ffd18d48be8150468f2bdd01d0bc7aa26557f817
Florian Kircher <flo.kircher@gmail.com>
2015-12-17 04:31:33 +0100

    acl for account model seperated for better customization

--- common/models/account.js
+++ common/models/account.js
@@ -1,3 +1,7 @@
 module.exports = function(Account) {
+  // disable acl from User model
   Account.settings.acls.length = 0;
+
+  // overwrite built in configuration
+  Account.settings.acls = require("./accountacl.json");
 };
