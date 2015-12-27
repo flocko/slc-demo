26582d868093cd8b664d93ff8a04cea7590bd268
Florian Kircher <flo.kircher@gmail.com>
2015-12-27 17:44:20 +0100

    players model

--- common/models/account.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(Account) {
-  // disable acl from User model
-  Account.settings.acls.length = 0;
-
-  // overwrite built in configuration
-  Account.settings.acls = require('./accountacl.json');
-};
